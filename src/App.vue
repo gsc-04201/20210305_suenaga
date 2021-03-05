595b5bb9e033072022a81a5cfc13315e721994f5
gsc-04201 <75969709+gsc-04201@users.noreply.github.com>
2021-03-05 16:22:47 +0900

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,330 @@
+<template>
+  <div id="firstview">
+    <CommonHeader></CommonHeader>
+    <p class="catchcopy">機械ではできない仕事を</p>
+      <div class="news flex">
+        <div class="news-content">
+          <p class="news_date">2019/01/01</p>
+          <p class="news_title">
+            ニュースタイトルがここには入ります。3月の決算報告書
+          </p>
+        </div>
+        <div class="news-icon">news</div>
+      </div>
+  </div>•
+  <main>
+    <div class="mission">
+      <div class="mission-content">
+        <h2 class="mission_title">
+          お客様のために進化する成長企業であり続ける
+        </h2>
+        <p class="mission_text">
+          顧客主導型サービスを提供する企業として、お客様の会社経営に必要なサービスと情報を提供し続ける企業支援会社を目指します。企業が現在直面している「勝ち残るため」の戦略を提供できる会社として、ホームページをはじめ、各種マーケティングをご提供いたします。
+        </p>
+        <a href="" class="common-btn mission-btn">ミッション詳細</a>
+      </div>
+      <div class="mission-img">
+        <img src="img/mission-img.jpg" alt="" />
+      </div>
+    </div>
+    <div class="service">
+      <h2 class="service-title section-title">service</h2>
+      <div class="service-content">
+        <div class="service-img">
+          <img src="img/service2.jpg" alt="" />
+        </div>
+        <div class="service-content_inner">
+          <h3 class="mgb-15">web制作事業</h3>
+          <p class="mgb-15">コーチングのミッション</p>
+          <p class="mgb-15">
+            ユーザーが求める物を分析し、ユーザビリティを備えた最適なサイトを提供。建設業から美容、ブライダル、飲食店まで多種多様な業種の制作実績があります
+          </p>
+          <a href="" class="common-btn service-btn">サービス詳細</a>
+        </div>
+      </div>
+      <div class="service-content">
+        <div class="service-img service-img_second">
+          <img src="img/service1.jpg" alt="" />
+        </div>
+        <div class="service-content_inner service-content_inner-second">
+          <h3 class="mgb-15">web制作事業</h3>
+          <p class="mgb-15">コーチングのミッション</p>
+          <p class="mgb-15">
+            ユーザーが求める物を分析し、ユーザビリティを備えた最適なサイトを提供。建設業から美容、ブライダル、飲食店まで多種多様な業種の制作実績があります
+          </p>
+          <a href="" class="common-btn service-btn">サービス詳細</a>
+        </div>
+      </div>
+    </div>
+    <div class="company">
+      <div class="recruit-company flex">
+        <div class="recruit-company_size company-img">
+          <h2 class="company-title section-title">
+            company<br /><span>会社概要</span>
+          </h2>
+        </div>
+        <div class="recruit-company_size recruit-img">
+          <h2 class="company-title section-title">
+            recruit<br /><span>採用情報</span>
+          </h2>
+        </div>
+      </div>
+    </div>
+    <div class="faq">
+      <h2 class="faq-title">FAQ</h2>
+      <p class="faq-title-jp">良くある質問</p>
+    </div>
+    <div class="contact">
+      <h2 class="contact-title section-title">
+        contact<br /><span>お問い合わせ</span>
+      </h2>
+      <p class="contact-subtitle section-title">
+        お気軽にお問い合わせください。
+      </p>
+      <p class="contact-text">
+        資料請求・お問い合わせなどなんでも気軽にお問い合わせください。お仕事のご連絡もお問い合わせページに飛んでいただいて、入力フォームにご記入お願いいたします。
+      </p>
+      <a href="" class="common-btn contact-btn">お問い合わせへ</a>
+    </div>
+  <CommonFooter></CommonFooter>
+
+  </main>
+
+
+
+</template>
+
+<script>
+import CommonHeader from "./components/CommonHeader.vue";
+import CommonFooter from "./components/CommonFooter.vue";
+export default {
+  components: {
+    CommonHeader,
+    CommonFooter
+  }
+};
+</script>
+
+
+
+
+
+<style>
+@charset "UTF-8";
+
+html, body, div, span, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+abbr, address, cite, code,
+del, dfn, em, img, ins, kbd, q, samp,
+small, strong, sub, sup, var,
+b, i,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, figcaption, figure,
+footer, header, hgroup, menu, nav, section, summary,
+time, mark, audio, video {
+  margin:0;
+  padding:0;
+  border:0;
+  outline:0;
+  font-size:100%;
+  vertical-align:baseline;
+  background:transparent;
+}
+
+body {
+  line-height:1;
+}
+
+article,aside,details,figcaption,figure,
+footer,header,hgroup,menu,nav,section {
+  display:block;
+}
+
+nav ul {
+  list-style:none;
+}
+
+blockquote, q {
+  quotes:none;
+}
+
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content:'';
+  content:none;
+}
+
+a {
+  margin:0;
+  padding:0;
+  font-size:100%;
+  vertical-align:baseline;
+  background:transparent;
+}
+
+/* change colours to suit your needs */
+ins {
+  background-color:#ff9;
+  color:#000;
+  text-decoration:none;
+}
+
+/* change colours to suit your needs */
+mark {
+  background-color:#ff9;
+  color:#000;
+  font-style:italic;
+  font-weight:bold;
+}
+
+del {
+  text-decoration: line-through;
+}
+
+abbr[title], dfn[title] {
+  border-bottom:1px dotted;
+  cursor:help;
+}
+
+table {
+  border-collapse:collapse;
+  border-spacing:0;
+}
+
+/* change border colour to suit your needs */
+hr {
+  display:block;
+  height:1px;
+  border:0;
+  border-top:1px solid #cccccc;
+  margin:1em 0;
+  padding:0;
+}
+
+input, select {
+  vertical-align:middle;
+}
+
+/* --------全体--------- */
+html {
+  height: 100%;
+}
+
+a {
+  color: #333333;
+  text-decoration: none;
+}
+
+img {
+  width: 100%;
+  height: 100%;
+  display: block;
+}
+
+.flex {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+body {
+  color: #333333;
+  height: 100%;
+}
+
+.mgb-15 {
+  margin-bottom: 15px;
+}
+
+.common-btn {
+  display: block;
+  width: 130px;
+  height: 40px;
+  line-height: 40px;
+  background: #fff;
+  padding: 0 20px;
+  color: #000;
+  text-align: center;
+}
+
+.section-title {
+  font-size: 40px;
+  line-height: 1;
+}
+
+.section-title span {
+  font-size: 20px;
+}
+
+/* --------header(footer)--------- */
+.header-logo {
+  font-size: 20px;
+  font-weight: bold;
+  margin-left: 35px;
+  color: #fff;
+  line-height: 70px;
+}
+
+.header-nav li {
+  display: inline-block;
+  margin-right: 35px;
+  font-size: 16px;
+}
+
+.header-nav a {
+  color: #fff;
+}
+
+#firstview {
+  height: 100%;
+  background: url(./assets/img/eyecatch.jpg) no-repeat;
+  background-size: cover;
+  background-position: center;
+  position: relative;
+}
+
+.catchcopy {
+  position: absolute;
+  top: 60%;
+  left: 10%;
+  font-size: 60px;
+  font-style: italic;
+  font-weight: bold;
+  color: #fff;
+  line-height: 1;
+}
+
+
+/* --------news--------- */
+.news {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 50%;
+}
+.news-content {
+  background: #fff;
+  width: 80%;
+  height: 70px;
+}
+
+.news_date {
+  padding-left: 5px;
+}
+
+.news_title {
+  padding: 0 15px;
+}
+.news-icon {
+  background: #000;
+  width: 20%;
+  height: 70px;
+  line-height: 70px;
+  text-align: center;
+  color: #fff;
+}
+
+</style>
